e0e79114dc5493bc8da68f4233a62fae29284dfe
Olaf <146707611+parowus1@users.noreply.github.com>
2023-11-10 23:18:42 +0100

    Stworzenie MergeSort.cpp oraz scalanie liczb

---
+++ MergeSort.cpp
@@ -0,0 +1,11 @@
+void MergeSort::merge(std::vector<int>& array, int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    std::vector<int> leftArray(n1);
+    std::vector<int> rightArray(n2);
+
+    for (int i = 0; i < n1; i++)
+        leftArray[i] = array[left + i];
+    for (int j = 0; j < n2; j++)
+        rightArray[j] = array[middle + 1 + j];
