d792efdc4522b5ea12d8e59c6fde623d702b1339
Olaf <146707611+parowus1@users.noreply.github.com>
2023-11-11 00:33:30 +0100

    Dodanie komentarzy

--- MergeSort.h
+++ MergeSort.h
@@ -5,11 +5,11 @@
 
 class MergeSort {
 public:
-    static void sort(std::vector<int>& array);
+    static void sort(std::vector<int>& array);  //do sortowania tablicy liczb całkowitych przekazanej jako argument
 
 private:
-    static void merge(std::vector<int>& array, int left, int middle, int right);
-    static void sort(std::vector<int>& array, int left, int right);
+    static void merge(std::vector<int>& array, int left, int middle, int right);  //Odpowiedzialna za scalanie dwóch podtablic w jedną posortowaną tablicę. Metoda ta przyjmuje cztery argumenty
+    static void sort(std::vector<int>& array, int left, int right);   //jest używana do rekurencyjnego sortowania tablicy w określonym zakresie indeksów od left do right.
 };
 
 #endif // MERGESORT_H
