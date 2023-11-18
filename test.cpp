bf9a35ed8d549fac61580a8b2dcde8ee44454423
Olaf <146707611+parowus1@users.noreply.github.com>
2023-11-18 17:23:28 +0100

    Test sprawdza, czy algorytm zachowuje tablicę niezmienioną, gdy ona jest już posortowana rosnąco

--- test.cpp
+++ test.cpp
@@ -1,3 +1,16 @@
 #include "pch.h"
 #include "C:\Users\parow\source\repos\Sort\MergeSort.cpp"
 
+TEST(PierwszyTest, TestNiezmienionaPosortowanaTablica) {
+    // Przykładowa posortowana rosnąco tablica
+    std::vector<int> array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    // Skopiuj tablicę przed sortowaniem
+    std::vector<int> originalArray = array;
+
+    // Wywołaj funkcję sortującą
+    MergeSort::sort(array);
+
+    // Sprawdź, czy tablica pozostała niezmieniona
+    EXPECT_EQ(originalArray, array);
+}
