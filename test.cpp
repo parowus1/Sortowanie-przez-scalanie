03fb67c5776bdc33bc24338cd5cafc13fcda9baa
Olaf <146707611+parowus1@users.noreply.github.com>
2023-11-18 17:27:57 +0100

    Test sprawdza, czy algorytm potrafi posortować tablicę, która jest posortowana w odwrotnej kolejności

--- test.cpp
+++ test.cpp
@@ -14,3 +14,16 @@ TEST(PierwszyTest, TestNiezmienionaPosortowanaTablica) {
     // Sprawdź, czy tablica pozostała niezmieniona
     EXPECT_EQ(originalArray, array);
 }
+
+TEST(DrugiTest, TestOdwrotniePosortowanaTablica) {
+    // Przykładowa tablica posortowana w odwrotnej kolejności
+    std::vector<int> array = { 9, 8, 7, 6, 5, 4, 3, 2, 2 };
+
+    // Wywołaj funkcję sortującą
+    MergeSort::sort(array);
+
+    // Sprawdź, czy tablica jest posortowana rosnąco
+    for (size_t i = 1; i < array.size(); ++i) { 
+        EXPECT_LE(array[i - 1], array[i]);  //
+    }
+}
